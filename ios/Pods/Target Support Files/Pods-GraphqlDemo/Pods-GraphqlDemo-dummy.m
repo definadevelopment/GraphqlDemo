b1d0e473b610c9c70a68aeb3cfd98d5960b0f1cc
Dan Defina <definadevelopment@gmail.com>
2020-02-19 08:42:18 -0500

    Server setup

---
+++ ios/Pods/Target Support Files/Pods-GraphqlDemo/Pods-GraphqlDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GraphqlDemo : NSObject
+@end
+@implementation PodsDummy_Pods_GraphqlDemo
+@end
---
+++ ios/Pods/Target Support Files/Pods-GraphqlDemoTests/Pods-GraphqlDemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GraphqlDemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_GraphqlDemoTests
+@end
